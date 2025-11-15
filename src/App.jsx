8483b90cf22c400526a1640198c94162e10da747
sujalshakya7 <sujalshakya777@gmail.com>
2025-11-15 20:31:42 +0545

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Blog from "./components/Blog";
-import Industries from "./components/Industries";
-import Pricing from "./components/Pricing";
-import Solutions from "./components/Solutions";
-import Tourism from "./components/Industries/Tourism";
-import { Navigate } from "react-router-dom";
-import TrekkingReport from "./components/Industries/TrekkingReport";
-import { Data } from "./components/Industries/Data";
 
 import Lenis from "@studio-freight/lenis";
+import AppRoutes from "./routes/AppRoutes";
+
 
 const App = () => {
   useEffect(() => {
@@ -37,21 +26,7 @@ const App = () => {
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/solutions" element={<Solutions />} />
-          <Route path="/industries" element={<Industries />} />
-
-          <Route path="/tourism" element={<Tourism />} />
-          <Route path="/trekking-reports" element={<TrekkingReport />} />
-          <Route path="/data" element={<Data />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+     <AppRoutes/>
     </>
   );
 };
